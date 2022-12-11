c72eb11ffa5739677c4a703ce41ca3c4d362f312
markirius <markbombomb@gmail.com>
2022-12-11 10:52:52 -0300

    updated gitignore, project's config files and added this README file.

---
+++ README.md
@@ -0,0 +1,22 @@
+# nodejs-api-sequelize-escola-ingles
+
+this project was made based on alura course [https://cursos.alura.com.br/course/orm-nodejs-api-sequelize-mysql](https://cursos.alura.com.br/course/orm-nodejs-api-sequelize-mysql)
+
+
+- API with NodeJS and some of its main libraries
+- Working ORM
+- Using Sequelize to make operations on databases without SQL
+- Following MVC structure
+- JavaScript back-end project
+- This example work with MYSQL database
+- Extends env variables usage
+
+
+`.env` content, put this file on project's' root directory:
+```
+DB_HOST=127.0.0.1
+DB_USER=root
+DB_PASS=dockerdatabase
+DATABASE=escola_ingles
+NODE_ENV=development
+```
--- api/config/config.js
+++ api/config/config.js
@@ -10,18 +10,18 @@ module.exports = {
     "operatorsAliases": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "operatorsAliases": false
   }
